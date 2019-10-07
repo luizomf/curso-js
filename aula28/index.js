298aaaf85c882875be20e52edc81f9f78990189f
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-07 17:43:41 -0300

    Aula 28 - Objeto Date

---
+++ aula28/index.js
@@ -0,0 +1,18 @@
+function zeroAEsquerda (num) {
+  return num >= 10 ? num : `0${num}`;
+} 
+
+function formataData(data) {
+  const dia = zeroAEsquerda(data.getDate());
+  const mes = zeroAEsquerda(data.getMonth() + 1);
+  const ano = zeroAEsquerda(data.getFullYear());
+  const hora = zeroAEsquerda(data.getHours());
+  const min = zeroAEsquerda(data.getMinutes());
+  const seg = zeroAEsquerda(data.getSeconds());
+
+  return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
