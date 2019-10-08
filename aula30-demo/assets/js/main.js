353f089b896a56aefd7370db28425f1cb087566c
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-07 23:15:42 -0300

    Aula 30

--- aula30-demo/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-const data = new Date();
-const opcoes = { 
-  dateStyle: 'full',
-  timeStyle: 'short'
-};
-
-const h1 = document.querySelector('.container h1');
-h1.innerHTML = data.toLocaleDateString('pt-BR', opcoes);
