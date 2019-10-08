bbc96ab8afd10120c810fa5579f589bc878dce0d
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-08 13:46:32 -0300

    Aula 31

---
+++ aula31/index.js
@@ -0,0 +1,37 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco { ... bloco }
+// Var só tem escopo de função
+
+// let nome = 'Luiz'; // criando
+// var nome2 = 'Luiz'; // criando
+
+
+// if (verdadeira) {
+//   let nome = 'Otávio'; // criando
+//   var nome2 = 'Rogério'; // redeclarando
+
+//   if (verdadeira) {
+//     var nome2 = 'Ronaldo'; // redeclarando
+//     let nome = 'Outra coisa';
+//   }
+// }
+
+// console.log(nome, nome2);  
+
+
+// function falaOi () {
+
+//   if (verdadeira) {
+//     let nome = 'Luiz';
+//     var sobrenome = 'Miranda';
+//   }
+
+//   console.log(sobrenome);
+
+// }
+
+// falaOi();
+
+let sobrenome = 'Miranda';
+console.log(sobrenome);
