e8a0d5a0b343c2df113634832db6cdd05d4493f3
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-08 08:57:42 -0300

    Aula 30 2

--- aula30/assets/js/main.js
+++ aula30/assets/js/main.js
@@ -2,77 +2,15 @@
 // const data = new Date();
 
 // function getDiaSemanaTexto(diaSemana) {
-//   let diaSemanaTexto;
-
-//   switch (diaSemana) {
-//   case 0:
-//     diaSemanaTexto = 'domingo';
-//     return diaSemanaTexto;
-//   case 1:
-//     diaSemanaTexto = 'segunda-feira';
-//     return diaSemanaTexto;
-//   case 2:
-//     diaSemanaTexto = 'terça-feira';
-//     return diaSemanaTexto;
-//   case 3:
-//     diaSemanaTexto = 'quarta-feira';
-//     return diaSemanaTexto;
-//   case 4:
-//     diaSemanaTexto = 'quinta-feira';
-//     return diaSemanaTexto;
-//   case 5:
-//     diaSemanaTexto = 'sexta-feira';
-//     return diaSemanaTexto;
-//   case 6:
-//     diaSemanaTexto = 'sábado';
-//     return diaSemanaTexto;
-//   default:
-//     diaSemanaTexto = '';
-//     return diaSemanaTexto;
-//   }
+//   const diasSemana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 
+//     'sexta', 'sábado'];
+//   return diasSemana[diaSemana];
 // }
 
 // function getNomeMes(numeroMes) {
-//   let nomeMes;
-
-//   switch (numeroMes) {
-//   case 0:
-//     nomeMes = 'janeiro';
-//     return nomeMes;
-//   case 1:
-//     nomeMes = 'fevereiro';
-//     return nomeMes;
-//   case 2:
-//     nomeMes = 'março';
-//     return nomeMes;
-//   case 3:
-//     nomeMes = 'abril';
-//     return nomeMes;
-//   case 4:
-//     nomeMes = 'maio';
-//     return nomeMes;
-//   case 5:
-//     nomeMes = 'junho';
-//     return nomeMes;
-//   case 6:
-//     nomeMes = 'julho';
-//     return nomeMes;
-//   case 7:
-//     nomeMes = 'agosto';
-//     return nomeMes;
-//   case 8:
-//     nomeMes = 'setembro';
-//     return nomeMes;
-//   case 9:
-//     nomeMes = 'outubro';
-//     return nomeMes;
-//   case 10:
-//     nomeMes = 'novembro';
-//     return nomeMes;
-//   case 11:
-//     nomeMes = 'dezembro';
-//     return nomeMes;
-//   }
+//   const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maior', 'junho', 
+//     'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+//   return meses[numeroMes];
 // }
 
 // function zeroAEsquerda (num) {
