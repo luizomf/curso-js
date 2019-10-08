e00bda1bc3feb5799f5d854110b63d56b37484b3
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-08 15:56:52 -0300

    Aula 32

---
+++ aula32/index.js
@@ -0,0 +1,6 @@
+// ... rest, ... spread
+//                    0          1          2
+//                 0  1  2    0  1  2    0  1  2
+const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+const [lista1, lista2, lista3] = numeros;
+console.log(lista3[2]);
