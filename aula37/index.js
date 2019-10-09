a13757d2ae828c7eb0a94b3923126e20d5704770
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-09 20:06:46 -0300

    Aula 37

---
+++ aula37/index.js
@@ -0,0 +1,30 @@
+const pessoa = {
+  nome: 'Luiz',
+  sobrenome: 'Otávio'
+};
+
+// For clássico - Geralmente com iteráveis (array ou strings)
+// For in - Retorna o índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si (iteráveis, arrays ou strings)
+
+// for (let i = 0; i < nomes.length; i++) {
+//   console.log(nomes[i]);
+// }
+
+// console.log('####');
+
+// for (let i in nomes) {
+//   console.log(nomes[i]);
+// }
+
+// console.log('####');
+
+// for (let valor of nomes) {
+//   console.log(valor);
+// }
+
+// console.log('####');
+
+// nomes.forEach(function(valor, indice, array) {
+//   console.log(valor, indice, array);
+// });
