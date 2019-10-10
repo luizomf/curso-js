9b23b83fe120032d860737f58b4f07f135d232e9
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-09 23:40:23 -0300

    Aula 38

---
+++ aula38/assets/js/main.js
@@ -0,0 +1,11 @@
+const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+console.log(backgroundColorBody);
+
+for (let p of ps) {
+  p.style.backgroundColor = backgroundColorBody;
+  p.style.color = '#FFFFFF';
+}
