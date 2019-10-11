f67e84fbe5ed6c1b175701f844937ce5ddb032d2
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-10 21:21:32 -0300

    Aula 46

---
+++ aula46/index.js
@@ -0,0 +1,19 @@
+function mostraHora() {
+  let data = new Date();
+
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false
+  });
+}
+
+const timer = setInterval(function() {
+  console.log(mostraHora());
+}, 1000);
+
+setTimeout(function() {
+  clearInterval(timer);
+}, 3000);
+
+setTimeout(function() {
+  console.log('Olá mundo!');
+}, 5000);
