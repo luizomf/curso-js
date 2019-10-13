674f37de11a356a0be2930309aba51cc9616dbe5
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-13 09:34:39 -0300

    Aula 57

---
+++ aula57/assets/js/main.js
@@ -0,0 +1,75 @@
+function criaCalculadora() {
+  return {
+    display: document.querySelector('.display'),
+
+    inicia() {
+      this.cliqueBotoes();
+      this.pressionaEnter();
+    },
+
+    pressionaEnter() {
+      this.display.addEventListener('keyup', e => {
+        if (e.keyCode === 13) {
+          this.realizaConta();
+        }
+      });
+    },
+
+    realizaConta() {
+      let conta = this.display.value;
+
+      try {
+        conta = eval(conta);
+
+        if(!conta) {
+          alert('Conta inválida');
+          return;
+        }
+
+        this.display.value = String(conta);
+      } catch(e) {
+        alert('Conta inválida');
+        return;
+      }
+    },
+
+    clearDisplay() {
+      this.display.value = '';
+    },
+
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
+
+    cliqueBotoes() {
+      document.addEventListener('click', e => {
+        const el = e.target;
+
+        if(el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText);
+        }
+
+        if(el.classList.contains('btn-clear')) {
+          this.clearDisplay();
+        }
+
+        if(el.classList.contains('btn-del')) {
+          this.apagaUm();
+        }
+
+        if(el.classList.contains('btn-eq')) {
+          this.realizaConta();
+        }
+      });
+    },
+
+    btnParaDisplay(valor) {
+      this.display.value += valor;
+    }
+
+  };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
