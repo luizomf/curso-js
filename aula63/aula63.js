c595bdef84638872f06698f52fe13f4c04d9137e
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-14 17:18:16 -0300

    Aula 63

---
+++ aula63/aula63.js
@@ -0,0 +1,15 @@
+//               -5       -4       -3         -2        -1
+//                0        1        2          3         4
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// Unshift
+nomes.splice(2, 0, 'Luiz', 'Otávio');
+console.log(nomes);
+
+// nomes.splice(índice atual, delete, elem1, elem2, elem3);
+// pop
+// const removidos = nomes.splice(-1, 1);
+
+// shift
+// const removidos = nomes.splice(0, 1);
+
