5dc2199face0be39c261111467571389f9fbe080
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-14 17:57:47 -0300

    Aula 64

---
+++ aula64/aula64.js
@@ -0,0 +1,6 @@
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
+// const a3 = a1.concat(a2, [7, 8, 9], 'Luiz');
+// ... rest -> ... spread
+const a3 = [...a1, 'Luiz', ...a2, ...[7, 8, 9]];
+console.log(a3);
