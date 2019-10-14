5ecbfd974063045005300f398226ce64f6a1b73c
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-14 15:22:01 -0300

    Aula 62

---
+++ aula62/index.js
@@ -0,0 +1,11 @@
+// Valor por referência
+//                 0         1        2        3          4
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+// const nome = 'Luiz Otávio Miranda';
+// const nomes = nome.split(' ');
+const nomes = [ 'Luiz', 'Otávio', 'Miranda' ];
+const nome = nomes.join(' ');
+console.log(nome);
