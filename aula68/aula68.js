6a58136ec626b31ccb312555a34a91c877581c70
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-15 09:44:36 -0300

    Aula 69

---
+++ aula68/aula68.js
@@ -0,0 +1,11 @@
+// Retorne a soma do dobro de todos os pares
+// -> Filtrar pares
+// -> Dobrar os valores
+// -> Reduzir (somar tudo)
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numerosPares = numeros
+  .filter(valor => valor % 2 === 0)
+  .map(valor => valor * 2)
+  .reduce((ac, valor) => ac + valor);
+
+console.log(numerosPares);
---
+++ aula69/aula69.js
@@ -0,0 +1,8 @@
+const a1 = [10, 20, 30];
+let total = 0;
+
+a1.forEach(valor => {
+  total += valor;
+});
+
+console.log(total);
