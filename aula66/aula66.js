56d1e1d05bf36fb5df89c50dd274b357f860cf2b
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-14 21:08:24 -0300

    Aula 66

---
+++ aula66/aula66.js
@@ -0,0 +1,29 @@
+// Dobre os números
+//               0  1   2   3  4 .....
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numerosEmDobro = numeros.map(valor => valor * 2);
+// console.log(numerosEmDobro);
+
+// Para cada elemento:
+// Retorne apenas uma string com o nome da pessoa
+// Remova apenas a chave "nome" do objeto
+// Adicione uma chave id em cada objeto (id)
+const pessoas = [
+  { nome: 'Luiz', idade: 62 },
+  { nome: 'Maria', idade: 23 },
+  { nome: 'Eduardo', idade: 55 },
+  { nome: 'Letícia', idade: 19 },
+  { nome: 'Rosana', idade: 32 },
+  { nome: 'Wallace', idade: 47 },
+];
+const nomes = pessoas.map(obj => obj.nome);
+const idades = pessoas.map(obj => ({ idade: obj.idade }));
+
+const comIds = pessoas.map(function(obj, indice) {
+  const newObj = { ...obj };
+  newObj.id = indice;
+  return newObj;
+});
+
+// console.log(pessoas);
+console.log(comIds);
