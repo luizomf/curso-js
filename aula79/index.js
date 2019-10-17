dfdb68f738e759ae40d59a00e4da48c16386b8d2
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-17 01:35:11 -0300

    Aula 79

---
+++ aula79/index.js
@@ -0,0 +1,30 @@
+const falar = {
+  falar() {
+    console.log(`${this.nome} está falando.`);
+  },
+};
+
+const comer = {
+  comer() {
+    console.log(`${this.nome} está comendo.`);
+  },
+};
+
+const beber = {
+  beber() {
+    console.log(`${this.nome} está bebendo.`);
+  },
+};
+
+const pessoaPrototype = Object.assign({}, falar, comer, beber);
+
+function criaPessoa(nome, sobrenome) {
+  return Object.create(pessoaPrototype, {
+    nome: { value: nome },
+    sobrenome: { value: sobrenome }
+  });
+}
+
+const p1 = criaPessoa('Luiz', 'Otávio');
+const p2 = criaPessoa('Maria', 'A.');
+console.log(p2);
