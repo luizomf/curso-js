967369850232bfcf64a991501bc4be28a2c49f63
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-19 02:46:47 -0300

    Aula 89

---
+++ aula89/assets/js/main.js
@@ -0,0 +1,46 @@
+const request = obj => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
+
+    xhr.addEventListener('load', () => {
+      if(xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    });
+  });
+};
+
+document.addEventListener('click', e => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if (tag === 'a') {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+});
+
+async function carregaPagina(el) {
+  const href = el.getAttribute('href');
+
+  const objConfig = {
+    method: 'GET',
+    url: href
+  };
+
+  try {
+    const response = await request(objConfig);
+    carregaResultado(response);
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = response;
+}
