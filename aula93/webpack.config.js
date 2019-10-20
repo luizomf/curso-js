c8573cf3046bb52f6f887905625d328d4a0cd4ea
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-20 14:27:48 -0300

    Aula 93

---
+++ aula93/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path'); // CommonJS
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }]
+  },
+  devtool: 'source-map'
+};
