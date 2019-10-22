63effb11ccc84184f67cf76d2f3f8404649ef483
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-21 21:11:00 -0300

    Deleted aula 95

--- aula95/src/main.js
+++
@@ -1,9 +0,0 @@
-import GeraCPF from './modules/GeraCPF';
-
-import './assets/css/style.css';
-
-(function renderizaCpf() {
-  const gera = new GeraCPF();
-  const cpfGerador = document.querySelector('.cpf-gerado');
-  cpfGerador.innerHTML = gera.geraNovoCpf();
-})();
