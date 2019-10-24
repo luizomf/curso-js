7b0a8c4bad8f91dadc2085db4aaada80ead5ad01
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-24 11:38:45 -0300

    Node -> Aula6 (Express)

---
+++ node/aula6/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+//         Criar   ler   atualizar apagar
+// CRUD -> CREATE, READ, UPDATE,   DELETE
+//         POST    GET   PUT       DELETE
+
+// http://meusite.com/ <- GET -> Entregue a página /
+// http://meusite.com/sobre <- GET -> Entregue a página /sobre
+// http://meusite.com/contato <- GET -> Entregue a página /contato
+
+app.get('/', (req, res) => {
+  res.send(`
+  <form action="/" method="POST">
+  Nome: <input type="text" name="nome">
+  <button>Enviar</button>
+  </form>
+  `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Recebi o formulário');
+});
+
+app.get('/contato', (req, res) => {
+  res.send('Obrigado por entrar em contato com a gente.');
+});
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
