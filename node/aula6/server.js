5e7ac0e039080b25a55961d5de32ce6ec19b88ab
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-24 12:14:26 -0300

    Introdução ao express

--- node/aula6/server.js
+++ node/aula6/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+
 //         Criar   ler   atualizar apagar
 // CRUD -> CREATE, READ, UPDATE,   DELETE
 //         POST    GET   PUT       DELETE
