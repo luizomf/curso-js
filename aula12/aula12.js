cc45e0f4b75d436dece2e3d1f4861a9fa8f35194
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-24 12:13:10 -0300

    Trocando valores entre variáveis

--- aula12/aula12.js
+++ aula12/aula12.js
@@ -2,6 +2,6 @@ let varA = 'A'; // B
 let varB = 'B'; // C
 let varC = 'C'; // A
 
-[varA, varB, varC] = [varB, varC, varA] 
+[varA, varB, varC] = [varB, varC, varA];
 
 console.log(varA, varB, varC);
