3682817f73fe172bf3cc48330680afd27c7e3495
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-24 12:13:56 -0300

    Exercício com numbers

--- aula14/index.js
+++ aula14/index.js
@@ -11,6 +11,7 @@ num1 = Number(num1.toFixed(2));
 console.log(num1.toFixed(2));
 console.log(Number.isInteger(num1));
 
+
 // console.log(num1.toString() + num2);
 // num1 = num1.toString();
 // console.log(num1.toString(2));
