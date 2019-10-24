1b9ac7bcc5bd4ab8f9245ab705c09c09bb9e7005
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-24 19:43:23 -0300

    Renderizando views no express

---
+++ node/aula10/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
+
