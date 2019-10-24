4255c5b577338a1d2b5a96e5fd0df90b4d929776
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-24 12:11:48 -0300

    Tipos de dados

--- aula9/index.js
+++ aula9/index.js
@@ -1,12 +1,13 @@
 // String, number, undefined, null, boolean
 const nome = 'Luiz'; // string
-const nome1 = "Luiz"; // string
-const nome2 = `Luiz`; // string
+const nome1 = 'Luiz'; // string
+const nome2 = 'Luiz'; // string
 const num1 = 10; // number
 const num2 = 10.52; // number
 let nomeAluno; // undefined -> não aponta pra local nenhuma na memória
 const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhuma na memória
 const aprovado = false; // Boolean = true, false (lógico)
+
 
 let a = 2;
 const b = a;
