c5c85bf14b11a3ddef9af04aa6aea147fa57c27e
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-24 14:52:28 -0300

    Parâmetros com o express

---
+++ node/aula8/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send(`
+  <form action="/" method="POST">
+  Nome do cliente: <input type="text" name="qualquercoisa"><br>
+  Outro campo: <input type="text" name="aquioutrocampo">
+  <button>Olá mundo</button>
+  </form>
+  `);
+});
+
+app.get('/testes/:idUsuarios?/:parametro?', (req, res) => {
+  // /profiles/3
+  // /profiles/?chave1=valor1&chave2=valor2&chave3=valor3
+  //
+  console.log(req.params);
+  console.log(req.query);
+  res.send(req.query.facebookprofile);
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(`O que você me enviou foi: ${req.body.qualquercoisa}`);
+});
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
+
