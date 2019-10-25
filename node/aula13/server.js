e32926c39f5a218a5d7a46de2a231d8ea1076231
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-25 11:11:03 -0300

    Express middlewares

---
+++ node/aula13/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// Nossos próprios middlewares
+app.use(middlewareGlobal);
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
+
---
+++ node/aula13/src/middlewares/middleware.js
@@ -0,0 +1,7 @@
+exports.middlewareGlobal = (req, res, next) => {
+  next();
+};
+
+exports.outroMiddleware = (req, res, next) => {
+  next();
+};
