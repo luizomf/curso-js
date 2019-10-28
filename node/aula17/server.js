cb2ea569df5fb33557935b30b6aaaf2e49e5cd8b
Luiz Otávio Miranda <luizomf@gmail.com>
2019-10-27 22:44:24 -0300

    Resumo

--- node/aula17/server.js
+++ node/aula17/server.js
@@ -17,7 +17,9 @@ const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
 app.use(helmet());
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
