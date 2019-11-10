557f29aed30e701828aa0e19ba5aacf8dd73c37f
Luiz Otávio Miranda <luizomf@gmail.com>
2019-11-10 16:06:30 -0300

    api_rest - nodemon + sucrase

---
+++ api_rest/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "no-console": "off"
+  },
+};
