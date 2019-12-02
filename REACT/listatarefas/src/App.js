fefc988091e23b0a50a8dcc4a89ed9df7f7b4a46
Otávio Miranda <luizomf@gmail.com>
2019-12-02 12:45:24 -0300

    Eliminando arquivos desnecessários do React

--- REACT/listatarefas/src/App.js
+++ REACT/listatarefas/src/App.js
@@ -1,26 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <h1>Hello world!</h1>;
 }
-
-export default App;
