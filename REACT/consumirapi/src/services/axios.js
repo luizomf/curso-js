5c3ed31005bcff4cae6448658ab6a8aeb0f77694
Otávio Miranda <luizomf@gmail.com>
2020-02-10 01:39:33 -0300

    Removed axios authorization header after logout

--- REACT/consumirapi/src/services/axios.js
+++ REACT/consumirapi/src/services/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://curso1.otaviomiranda.com.br',
+  baseURL: 'http://localhost:3001',
 });
--- REACT/consumirapi/src/store/modules/auth/reducer.js
+++ REACT/consumirapi/src/store/modules/auth/reducer.js
@@ -1,4 +1,5 @@
 import * as types from '../types';
+import axios from '../../../services/axios';
 
 const initialState = {
   isLoggedIn: false,
@@ -19,6 +20,7 @@ export default function(state = initialState, action) {
     }
 
     case types.LOGIN_FAILURE: {
+      delete axios.defaults.headers.Authorization;
       const newState = { ...initialState };
       return newState;
     }
