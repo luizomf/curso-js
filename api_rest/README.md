399e73e88f52fd5d9ce317b483e5add73d5394bf
Otávio Miranda <luizomf@gmail.com>
2020-04-30 15:37:21 -0300

    Mudança da base de dados para SQLite, para não ser necessário servidor.

---
+++ api_rest/README.md
@@ -0,0 +1,10 @@
+# Uma API REST simples para consumo de dados
+
+Para subir o projeto no ar com SQLite, simplesmente execute os comandos abaixo:
+
+```
+npm i
+npx sequelize db:migrate
+npx sequelize db:seed:all
+npm run dev
+```
--- api_rest/src/config/database.js
+++ api_rest/src/config/database.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: 'mariadb',
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
+  /* SQLite */
+  dialect: 'sqlite',
+  storage: './db.sqlite',
+
+  /* MySQL / MariaDB */
+  // host: process.env.DATABASE_HOST,
+  // port: process.env.DATABASE_PORT,
+  // username: process.env.DATABASE_USERNAME,
+  // password: process.env.DATABASE_PASSWORD,
+  // database: process.env.DATABASE,
+  // dialectOptions: {
+  //   timezone: 'America/Sao_Paulo',
+  // },
+  // timezone: 'America/Sao_Paulo',
+
+  /* ALL */
   define: {
     timestamps: true,
     underscored: true,
@@ -14,8 +24,4 @@ module.exports = {
     createdAt: 'created_at',
     updatedAt: 'updated_at',
   },
-  dialectOptions: {
-    timezone: 'America/Sao_Paulo',
-  },
-  timezone: 'America/Sao_Paulo',
 };
