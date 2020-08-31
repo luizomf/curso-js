5a14983a8cbe1f9e12d927ac82e00d7274cff2fe
Otávio Miranda <luizomf@gmail.com>
2020-08-31 13:23:05 -0300

    Update Packages

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -13,14 +13,14 @@ exports.register = async function(req, res) {
     if(login.errors.length > 0) {
       req.flash('errors', login.errors);
       req.session.save(function() {
-        return res.redirect('back');
+        return res.redirect('/login/index');
       });
       return;
     }
 
     req.flash('success', 'Seu usuário foi criado com sucesso.');
     req.session.save(function() {
-      return res.redirect('back');
+      return res.redirect('/login/index');
     });
   } catch(e) {
     console.log(e);
