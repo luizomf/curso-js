2df082abc76a6650045cfd38615b9d86a1555e3d
Otávio Miranda <luizomf@gmail.com>
2021-02-06 18:03:08 -0300

    Upgrade packages

--- api_rest/dist/database/seeds/20191113015433-criar-usuarios.js
+++ api_rest/dist/database/seeds/20191113015433-criar-usuarios.js
@@ -5,23 +5,9 @@ module.exports = {
     'users',
     [
       {
-        nome: 'Luiz',
-        email: 'luiz1@gmail.com',
+        nome: 'admin',
+        email: 'admin@email.com',
         password_hash: await bcryptjs.hash('123456', 8),
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        nome: 'Luiz 2',
-        email: 'luiz2@gmail.com',
-        password_hash: await bcryptjs.hash('654321', 8),
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        nome: 'Luiz 3',
-        email: 'luiz3@gmail.com',
-        password_hash: await bcryptjs.hash('147852369', 8),
         created_at: new Date(),
         updated_at: new Date(),
       },
