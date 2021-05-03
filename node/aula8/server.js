d2a470d79395a2459077c6dfe5f764c7d8a8166d
Otávio Miranda <luizomf@gmail.com>
2021-05-03 15:18:31 -0300

    Upgrade

--- node/aula8/server.js
+++ node/aula8/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`
